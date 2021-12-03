3144f9abab09d5cf08d858a6d91d764025ebff86
박찬진 <68864422+developmentviusal@users.noreply.github.com>
2021-12-04 03:13:53 +0900

    first setting

---
+++ src/main/java/com/example/SendingToPro/SendingToProApplication.java
@@ -0,0 +1,13 @@
+package com.example.SendingToPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendingToProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendingToProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SendingToPro/SendingToProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SendingToPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendingToProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
